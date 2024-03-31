106fd890b10eab1e506c6019a6523b68acfa6832
obbteam <obbteam12@gmail.com>
2024-03-31 17:30:20 +0200

    default homepage + hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import {LoginState} from "./context/loginContext";
 
 
@@ -9,9 +8,9 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <LoginState>
             <App/>
         </LoginState>
-    </BrowserRouter>
+    </HashRouter>
 );
